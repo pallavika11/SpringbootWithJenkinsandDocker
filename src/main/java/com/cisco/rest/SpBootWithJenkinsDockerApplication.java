034e997626bdf2a14337c89fc53e0ecd50bb5cfc
pallavika11 <gampalapallavika@gmail.com>
2024-03-01 10:18:57 +0530

    first commit

---
+++ src/main/java/com/cisco/rest/SpBootWithJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.cisco.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpBootWithJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpBootWithJenkinsDockerApplication.class, args);
+	}
+
+}
